864de8425d0166b4d01ee48ad990aa9557db02df
MSLKW <maxwell.liew06@gmail.com>
2026-06-11 12:50:45 +0800

    feat(profiler): Implemented profiler to profile gold database
    feat(all): implemented all command for running the whole ETL pipeline
    
    chore: cleared up some comments and unnecessary functions

--- week_1/main.py
+++ week_1/main.py
@@ -3,40 +3,20 @@ from pathlib import Path
 from src.ingestor import ingest_all_mhtml
 from src.processor import process_all_html
 from src.loader import load_all_jsons
-# from src.run_data_profile import run_data_profile
+from src.profiler import run_data_profile
 
 SOURCE_DIR = Path("data/0_source")
 BRONZE_DIR = Path("data/1_bronze")
 SILVER_DIR = Path("data/2_silver")
 GOLD_DIR = Path("data/3_gold")
 DB_NAME = "jobs.db"
-
-# def run_profiler():
-# 	db_path = GOLD_DIR/DB_NAME
-# 	run_data_profile(db_path)
-
-# def run_gold():
-# 	input_dir = SILVER_DIR
-# 	output_dir = GOLD_DIR
-# 	load_all_jsons(input_dir, output_dir)
-
-# def run_silver():
-# 	input_dir = BRONZE_DIR
-# 	output_dir = SILVER_DIR
-# 	process_all_html(input_dir, output_dir)
-
-def run_bronze():
-	input_dir = SOURCE_DIR
-	output_dir = BRONZE_DIR
-	ingest_all_mhtml(input_dir, output_dir)
     
 def main():
 	total_args = len(sys.argv)
 	if (total_args != 2):
-		print("Error: wrong number of arguments", file=sys.stderr)
+		print("Usage: python main.py [ingest|process|load|profile|all]")
 		sys.exit(1)
 	argument = sys.argv[1]
-	# use match/case
 	match (argument):
 		case "ingest":
 			print("🥉 Bronze: Running \"ingest\" command")
@@ -45,10 +25,22 @@ def main():
 			print("🥈 Silver: Running \"process\" command")
 			process_all_html(BRONZE_DIR, SILVER_DIR)
 		case "load":
-			print("Gold: Running \"load\" command")
+			print("🥇 Gold: Running \"load\" command")
 			load_all_jsons(SILVER_DIR, GOLD_DIR / DB_NAME)
+		case "profile":
+			print("🔍 Profile: Running \"profile\" command")
+			run_data_profile(GOLD_DIR / DB_NAME)
+		case "all":
+			print("🌏 All: Running \"all\" command")
+			ingest_all_mhtml(SOURCE_DIR, BRONZE_DIR)
+			print("-----")
+			process_all_html(BRONZE_DIR, SILVER_DIR)
+			print("-----")
+			load_all_jsons(SILVER_DIR, GOLD_DIR / DB_NAME)
+			print("-----")
+			run_data_profile(GOLD_DIR / DB_NAME)
 		case _:
-			print(f"Error: Unknown command: {argument}")
+			print("Usage: python main.py [ingest|process|load|profile|all]")
 	sys.exit(0)
 
 	
