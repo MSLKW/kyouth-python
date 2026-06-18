6609b855f9d55899f66ae56861c47691b4398f71
MSLKW <maxwell.liew06@gmail.com>
2026-06-18 18:00:31 +0800

    refactor(prompt_model): made it so that error handling should be managed by calling function
    feat(tag_data): tag_data can now be performed in batches

--- week_2/prompt_model.py
+++ week_2/prompt_model.py
@@ -5,36 +5,33 @@ import sys
 import os
 import dotenv
 
+# Must use a try except block to catch error exceptions
 def prompt_model(model: str, prompt: str) -> str :
 	if (model == "gemini-2.5-flash" or model == "gemini-2.5-flash-lite" or model == "gemini-3-flash-preview"):
-		try:
-			dotenv.load_dotenv()
-			client = genai.Client(api_key=os.environ.get("API_KEY"))
-			response = client.models.generate_content(
-				model=model,
-				contents=prompt
-			)
-		except ValueError as e:
-			return (e)
-		except Exception as e:
-			return (e)
+		dotenv.load_dotenv()
+		client = genai.Client(api_key=os.environ.get("API_KEY"))
+		response = client.models.generate_content(
+			model=model,
+			contents=prompt
+		)
 		return (response.text)
 
-	try:
-		ollama_response = ollama.generate(
-			model=model,
-			prompt=prompt
-		)
-		return (ollama_response['response'])
-	except ResponseError as e:
-		return (e)
+	ollama_response = ollama.generate(
+		model=model,
+		prompt=prompt
+	)
+	return (ollama_response['response'])
 
 def main():
 	if (len(sys.argv) != 3):
 		print("Usage: uv run prompt_mode.py <model> <prompt>")
 		sys.exit(1)
-
-	print(f"\n--- RESPONSE ---\n\n{prompt_model(sys.argv[1], sys.argv[2])}")
+	
+	try:
+		result = prompt_model(sys.argv[1], sys.argv[2])
+	except Exception as e:
+		print("Encountered an error when prompting the model")
+	print(f"\n--- RESPONSE ---\n\n{result}")
     
         
 if __name__ == "__main__":
