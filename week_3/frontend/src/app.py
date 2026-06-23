dedafa0e58e551776b45ab94eb56d725097048f1
MSLKW <maxwell.liew06@gmail.com>
2026-06-23 23:41:58 +0800

    fix(frontend): Fixed up app.py so that docker can run properly now

--- week_3/frontend/src/app.py
+++ week_3/frontend/src/app.py
@@ -1,10 +1,13 @@
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from pathlib import Path
 
 app = FastAPI()
 
-templates = Jinja2Templates("templates")
+CURRENT_FILE_DIR = Path(__file__).resolve().parent
+
+templates = Jinja2Templates(CURRENT_FILE_DIR / "templates")
 
 @app.get('/', response_class=HTMLResponse)
 def get_root(request: Request):
